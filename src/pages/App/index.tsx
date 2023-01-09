9de4a008786d91469d8b7ec203ae85ca595c37e1
xia0hj <719887163@qq.com>
2023-01-09 21:29:43 +0800

    feat: ui (WIP)

--- src/pages/App/index.tsx
+++ src/pages/App/index.tsx
@@ -1,9 +1,9 @@
-import React from "react";
+import React, { MouseEventHandler, useState } from "react";
 import { addGame, Game, selectGames } from "@/store/gameLibrarySlice";
 import { useAppDispatch, useAppSelector } from "@/store";
-import {Button, Menu} from 'antd';
+import { Button, Menu, Tooltip, Modal, Typography } from 'antd';
 import type { MenuProps } from 'antd';
-import {FileAddOutlined, SettingOutlined} from '@ant-design/icons'
+import { FileAddOutlined, SettingOutlined, EditOutlined } from '@ant-design/icons'
 import styles from './styles.module.scss'
 
 const topMenuConfig: MenuProps['items'] = [
@@ -21,6 +21,7 @@ const topMenuConfig: MenuProps['items'] = [
 
 function App() {
 
+  const [curGameDetail, setCurGameDetail] = useState<Game>();
   const games = useAppSelector(selectGames);
   const dispatch = useAppDispatch()
 
@@ -36,26 +37,53 @@ function App() {
     })
   }
 
-  function mapGamesToSiderMenuItems(games:Array<Game>): MenuProps['items'] {
-    return games.map(game=>({
+  function mapGamesToSiderMenuItems(games: Array<Game>): MenuProps['items'] {
+    return games.map(game => ({
       label: game.properties.title,
       key: game.properties.title
     }))
   }
 
-  const onTopMenuBarClick: MenuProps['onClick'] = function(e){
-    switch(e.key){
-      case 'addGame':{
+  const onTopMenuBarClick: MenuProps['onClick'] = function (e) {
+    switch (e.key) {
+      case 'addGame': {
         clickAddGame();
       }
     }
   }
 
+  const onSiderBarClick: MenuProps['onClick'] = function (e) {
+    setCurGameDetail(games.find(game => game.properties.title === e.key))
+  }
+
+  function onEditPropertiesClick() {
+    Modal.confirm({
+      title: '编辑属性'
+    })
+  }
+
+  function renderGameDetail() {
+    if (curGameDetail) {
+      return (
+        <div className={styles.detail_top}>
+          <Typography.Title className={styles.title}>{curGameDetail.properties.title}</Typography.Title>
+          <Tooltip title="编辑属性">
+            <EditOutlined className={styles.edit_icon} onClick={onEditPropertiesClick} />
+          </Tooltip>
+        </div>
+      )
+    }
+  }
+
   return (
     <div className={styles.container}>
-      <Menu items={topMenuConfig} mode="horizontal" selectable={false} onClick={onTopMenuBarClick}/>
+      <Menu items={topMenuConfig} mode="horizontal" selectable={false} onClick={onTopMenuBarClick} />
       <div className={styles.content}>
-        <Menu items={mapGamesToSiderMenuItems(games)} mode="vertical" />
+        <Menu items={mapGamesToSiderMenuItems(games)} mode="vertical" onClick={onSiderBarClick} />
+        <div className={styles.detail}>
+          <a />
+          {renderGameDetail()}
+        </div>
       </div>
     </div>
   )
