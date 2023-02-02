fdb00faaccd80132e5cc3c9b10b9c324ca17532e
xia0hj <719887163@qq.com>
2023-02-03 00:39:28 +0800

    refactor: remove useless files

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './pages/app'
-import './samples/node-api'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
