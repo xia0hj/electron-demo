8c6cf24e1e06811e97aaf9aa869dcd7c13be6948
xia0hj <719887163@qq.com>
2023-02-08 00:32:09 +0800

    feat: check the window is foreground

--- electron/native/process-observer.ts
+++ electron/native/process-observer.ts
@@ -1,8 +1,10 @@
-import { execFile, ChildProcess } from 'node:child_process';
+import { execFile, ChildProcess, spawn } from 'node:child_process';
 import { stat } from 'node:fs/promises'
 import { User32 } from 'win32-api/promise';
-import {DTypes} from 'win32-api'
+import { DTypes } from 'win32-api'
 import ffi from 'ffi-napi'
+
+import { testEventHook } from './testWin32';
 
 export class ProcessObserver {
 
@@ -14,19 +16,24 @@ export class ProcessObserver {
   constructor(path: string) {
     this.startTime = Date.now();
     this.execProcess = execFile(path);
-    this.execProcess.on('exit', ()=>{
+    this.execProcess.on('exit', () => {
       const exitTime = Date.now();
       console.log('进程结束: ', {
         exitTime,
-        duration: `${(exitTime-this.startTime)/1000}秒`
+        duration: `${(exitTime - this.startTime) / 1000}秒`
       })
     })
 
     console.log('进程启动: ', {
-      pid:this.execProcess.pid,
+      pid: this.execProcess.pid,
       spawnfile: this.execProcess.spawnfile,
       startTime: this.startTime
     })
+
+    if (this.execProcess.pid !== undefined) {
+      testEventHook(this.execProcess.pid);
+    }
+
   }
 
 
