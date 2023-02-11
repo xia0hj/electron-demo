4dde532b9992a8938e288bb0d3e45e858c4ed5c4
xia0hj <719887163@qq.com>
2023-02-11 13:02:01 +0800

    feat: process-observer

---
+++ src/main/native/process-observer.ts
@@ -0,0 +1,44 @@
+import { execFile, ChildProcess, spawn } from 'node:child_process';
+import { stat } from 'node:fs/promises'
+import { setTimeout, clearTimeout } from 'node:timers'
+
+import activeWindow from 'active-win';
+
+export class ProcessObserver {
+  execProcess: ChildProcess;
+
+  startTime: number;
+
+  timer: NodeJS.Timeout;
+
+
+  constructor(path: string) {
+    this.startTime = Date.now();
+    this.execProcess = execFile(path);
+    this.execProcess.on('exit', () => {
+      const exitTime = Date.now();
+      clearTimeout(this.timer);
+      console.log('进程结束: ', {
+        exitTime,
+        duration: `${(exitTime - this.startTime) / 1000}秒`
+      })
+    })
+
+    console.log('进程启动: ', {
+      pid: this.execProcess.pid,
+      spawnfile: this.execProcess.spawnfile,
+      startTime: this.startTime
+    })
+
+    const monitorHandler = async () => {
+      const curWindow = await activeWindow();
+      if (curWindow?.owner.processId === this.execProcess.pid) {
+        console.log('正在运行');
+      } else {
+        console.log('后台运行')
+      }
+      this.timer = setTimeout(monitorHandler, 1000);
+    }
+    this.timer = setTimeout(monitorHandler, 1000);
+  }
+}
--- src/main/preload.ts
+++ src/main/preload.ts
@@ -2,11 +2,12 @@ import { contextBridge, dialog } from "electron";
 import path from 'node:path'
 import activeWindow from "active-win";
 import { ipcEventSender } from "./native/electron-ipc";
+import { ProcessObserver } from "./native/process-observer";
 
 const nativeApi = {
   ...ipcEventSender,
   run(path:string){
-    console.log('run: ',path)
+    new ProcessObserver(path);
   }
 }
 
